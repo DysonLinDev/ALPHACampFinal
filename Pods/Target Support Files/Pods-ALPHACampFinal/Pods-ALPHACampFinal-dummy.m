bd9c8f17b69e42478926a754354fd1b9ee8e6a10
Dyson <linshihting1029@gmail.com>
2016-04-29 15:01:44 +0800

    Add pod FireBase

---
+++ Pods/Target Support Files/Pods-ALPHACampFinal/Pods-ALPHACampFinal-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ALPHACampFinal : NSObject
+@end
+@implementation PodsDummy_Pods_ALPHACampFinal
+@end
---
+++ Pods/Target Support Files/Pods-ALPHACampFinal/Pods-ALPHACampFinal-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ALPHACampFinalVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ALPHACampFinalVersionString[];
+
