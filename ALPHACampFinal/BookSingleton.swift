0b65095c69d5acb70ad44f30c82fb4045d467129
Dyson <linshihting1029@gmail.com>
2016-04-29 15:01:13 +0800

    Add class to save Data

---
+++ ALPHACampFinal/BookSingleton.swift
@@ -0,0 +1,18 @@
+//
+//  BookSingleton.swift
+//  ALPHACampFinal
+//
+//  Created by MBPrDyson on 4/29/16.
+//  Copyright © 2016 MBPrDyson. All rights reserved.
+//
+
+import Foundation
+
+
+class Books {
+    var bookArray:[Book]?
+    
+    
+    
+    static let myBooks = Books()
+}
