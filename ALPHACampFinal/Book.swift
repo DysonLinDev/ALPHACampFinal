908c8f68f97bba78953b814cc2e6e5f4d128f4f9
Dyson <linshihting1029@gmail.com>
2016-04-29 14:40:25 +0800

    Add class Book

---
+++ ALPHACampFinal/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  ALPHACampFinal
+//
+//  Created by MBPrDyson on 4/29/16.
+//  Copyright © 2016 MBPrDyson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Book {
+    var bookName:String?
+    var bookPic:UIImage?
+    var bookShopAdd:String?
+    var bookIntrUrl:String?
+    var bookInfo:String?
+}
